3ef2d0e37fc9a6369d97a565e765e73a3864a114
dheerajpawar <60095236+dheerajnpawar@users.noreply.github.com>
2022-04-14 22:36:08 +0530

    Add files via upload

---
+++ gcdrecursion.c
@@ -0,0 +1,29 @@
+int gcd(int a, int b)
+{
+    if (a == 0)
+        return b;
+    return gcd(b % a, a);
+}
+
+
+int findGCD(int arr[], int n)
+{
+    int result = arr[0];
+    for (int i = 1; i < n; i++)
+        result = gcd(arr[i], result);
+
+    return result;
+}
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int t=findGCD(arr,n);
+    printf("%d",t);
+}
